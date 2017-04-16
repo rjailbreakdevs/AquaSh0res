ad8243292f42826ca3921e76c22f45049cbb80ed
EthanRDoesMC <EthanRDoesMC@gmail.com>
2017-04-15 20:54:46 -0400

    Initial Commit

---
+++ AquaSh0res/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AquaSh0res
+//
+//  Created by EthanR on 4/15/17.
+//  Copyright © 2017 r/jailbreak Devs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AquaSh0res/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AquaSh0res
+//
+//  Created by EthanR on 4/15/17.
+//  Copyright © 2017 r/jailbreak Devs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
